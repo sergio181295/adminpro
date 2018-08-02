f03bf40c8e8e897033816d5ee19a89b05a9c687c
sguillen <sguillen@cads.com.gt>
2018-08-02 15:12:47 -0600

    Se separaron las rutas de pages del app.modulo y se creo un pages.router para modular las rutas
    se importo el pages.router en al pages.module
    
    se eliminaron los comentareos basuras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,6 @@ import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NotpagefoundComponent } from './shared/notpagefound/notpagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
@@ -24,15 +16,7 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    // DashboardComponent,
-    // ProgressComponent,
-    // Graficas1Component,
-    // PagesComponent,
-    // HeaderComponent,
-    // SidebarComponent,
-    // BreadcrumbsComponent,
-    // NotpagefoundComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,6 +5,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from './pages.routes';
 
 @NgModule({
     declarations: [
@@ -18,8 +19,9 @@ import { SharedModule } from '../shared/shared.module';
         ProgressComponent,
         Graficas1Component
     ],
-    imports:[
-        SharedModule
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
     ]
 })
 export class PagesModule { }
